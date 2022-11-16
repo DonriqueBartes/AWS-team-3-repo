424fabcb24ba00cb87dd1865342f82ad9ba7739b
Kenan West <rinzlerwest@gmail.com>
2022-11-16 18:42:32 +0200

    T3-30

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 async function findFood(selectedValue)
 {
-        let data = await fetch(`./files/${selectedValue}.json`);
-        let foodChoice = await data.json();
-        let foodHolder = document.querySelector("#data-output");
+        const data = await fetch(`./files/${selectedValue}.json`);
+        const foodChoice = await data.json();
+        const foodHolder = document.querySelector("#data-output");
         let output = "";
         
-        for(let place of foodChoice){
+        for(const place of foodChoice){
             output += ` 
             <div id ="content-open">
                 <div class = "card">                                       
@@ -26,32 +26,50 @@ async function findFood(selectedValue)
 
 function reply_click(clicked_id)
 {
-    let placeHolder = document.querySelector("#output-stuff");
+    const placeHolder = document.querySelector("#output-stuff");
     placeHolder.innerHTML = `
-    <div class = "container">
-        <div class="modal fade" id="myModal" role="dialog">
-            <div class="modal-dialog">
-            
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-
-                        <h4 style="color: black;"><b>Where can you find this place?</b></h4>
-                        </div>
-                            <div class="modal-body">
-                                <p>
-                                <iframe src="${clicked_id}" width="550" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                                </p>
-                            </div>
-                        <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
+        <div class="overlay" id="this-overlay"></div>
+        <div class = "modal-container" id="this-modal">
+        Where can you find this place?
+            <div class="modal-body">
+                <p>
+                <iframe src="${clicked_id}" width="700" height="450vh" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </p>
             </div>
+            <button type="button" id="close-this" class="close-default">Close</button>
         </div>
-    </div>`;
+    `;
+
+    const targetDiv = document.getElementById("this-overlay");
+    const targetModal = document.getElementById("this-modal");
+    const btn = document.getElementById("close-this");
+    //---Close modal if close button is pressed 
+    btn.onclick =function() 
+    {
+        
+        if (targetDiv.style.display !== "none"){
+            targetDiv.style.display = "none";
+            targetModal.style.display = "none";
+        } else {
+            targetDiv.style.display = "block";
+        }
+    };
+    //---Close modal if 'Escape' key is pressed
+    document.onkeydown = function(evt) {
+        evt = evt || window.event;
+        var isEscape = false;
+        if ("key" in evt) {
+            isEscape = (evt.key === "Escape" || evt.key === "Esc");
+        } else {
+            isEscape = (evt.keyCode === 27);
+        }
+        if (isEscape) {
+            targetDiv.style.display = "none";
+            targetModal.style.display = "none";
+        }
+    };
 }
+
 
 function openForm() {
     document.getElementById("loginForm").style.display = "block";
